6603c27ae9d98b6b93ff439921214fbe25320063
stivdiaz <jstivendelarue@gmail.com>
2026-05-28 09:07:31 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, send_file
 import sqlite3
 app=Flask(__name__)
 DB='database.db'
